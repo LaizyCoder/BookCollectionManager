f72ed790971a7a583b54140ea399f76636d24ce2
RAHUL <140370820+LaizyCoder@users.noreply.github.com>
2026-06-26 12:57:20 +0530

    Implement BookListActivity with RecyclerView setup

--- java/com/example/bookcollectionmanager/BookListActivity.kt
+++ java/com/example/bookcollectionmanager/BookListActivity.kt
@@ -1,1 +1,29 @@
 // BookListActivity placeholder
+
+// Student ID: 2024EB01088
+
+package com.example.bookcollectionmanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class BookListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book_list)
+
+        val recycler = findViewById<RecyclerView>(R.id.recyclerView)
+
+        recycler.layoutManager = LinearLayoutManager(this)
+
+        val db = DatabaseHelper(this)
+
+        recycler.adapter = BookAdapter(db.getAllBooks())
+
+    }
+
+}
